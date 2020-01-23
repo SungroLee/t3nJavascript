b29e249ada068f05dd7d17c517bdd833a10f0cf0
Danz <kaiadrian.danz@baur.de>
2020-01-23 15:43:23 +0100

    implementing default Pionier werden section

--- js/app.js
+++ js/app.js
@@ -1,80 +1,80 @@
-/********************************
- * Navbar
- *********************************/
-//////////////// Variable/////////////////
-// Navbar
-const nav = document.createElement("nav"); //navbar
-const navLogoDiv = document.createElement("div"); //nav left div
-const navLoginDiv = document.createElement("div"); //nav right div
-const navLogoFoto = document.createElement("img"); //logo img
-const navText = document.createElement("a"); // Nav Right Text "t3n"
-const navButton = document.createElement("a"); // Login Button in Nav
-// Pioneers Network
-const pioneersDiv = document.createElement("div");
-const pioneersH1 = document.createElement("h1");
-const pioneersH2 = document.createElement("h2");
-const pioneersButton = document.createElement("a");
-// Slider
-const sliderDefaultDiv = document.createElement("div");
-const sliderH2 = document.createElement("h2");
-const sliderDiv = document.createElement("div");
-const slider = document.createElement("div");
-const leftBtn = document.createElement("button");
-const rightBtn = document.createElement("button");
-///////////////Atribute//////////////////
-navLogoDiv.id = 'logoDiv';
-navLoginDiv.id = 'loginDiv';
-navLogoFoto.id = 'logoFoto';
-navText.id = 'navt3n';
-navButton.id = 'loginButton';
-navLogoFoto.src = "./foto/t3n-logo.png";
-navText.href = '#';
-navButton.href = '#';
-// Pioneers Network
-pioneersDiv.id = 'pioneersDiv';
-pioneersH1.id = 'pioneersH1';
-pioneersButton.id = 'pioneersButton';
-pioneersButton.href = '#';
-// Slder
-sliderDefaultDiv.id = 'sliderDefaultDiv';
-sliderDiv.id = 'sliderDiv';
-slider.id = "slider";
-leftBtn.id = 'leftBtn';
-leftBtn.id = 'rightBtn';
+// /********************************
+//  * Navbar
+//  *********************************/
+// //////////////// Variable/////////////////
+// // Navbar
+// const nav = document.createElement("nav"); //navbar
+// const navLogoDiv = document.createElement("div"); //nav left div
+// const navLoginDiv = document.createElement("div"); //nav right div
+// const navLogoFoto = document.createElement("img"); //logo img
+// const navText = document.createElement("a"); // Nav Right Text "t3n"
+// const navButton = document.createElement("a"); // Login Button in Nav
+// // Pioneers Network
+// const pioneersDiv = document.createElement("div");
+// const pioneersH1 = document.createElement("h1");
+// const pioneersH2 = document.createElement("h2");
+// const pioneersButton = document.createElement("a");
+// // Slider
+// const sliderDefaultDiv = document.createElement("div");
+// const sliderH2 = document.createElement("h2");
+// const sliderDiv = document.createElement("div");
+// const slider = document.createElement("div");
+// const leftBtn = document.createElement("button");
+// const rightBtn = document.createElement("button");
+// ///////////////Atribute//////////////////
+// navLogoDiv.id = 'logoDiv';
+// navLoginDiv.id = 'loginDiv';
+// navLogoFoto.id = 'logoFoto';
+// navText.id = 'navt3n';
+// navButton.id = 'loginButton';
+// navLogoFoto.src = "./foto/t3n-logo.png";
+// navText.href = '#';
+// navButton.href = '#';
+// // Pioneers Network
+// pioneersDiv.id = 'pioneersDiv';
+// pioneersH1.id = 'pioneersH1';
+// pioneersButton.id = 'pioneersButton';
+// pioneersButton.href = '#';
+// // Slder
+// sliderDefaultDiv.id = 'sliderDefaultDiv';
+// sliderDiv.id = 'sliderDiv';
+// slider.id = "slider";
+// leftBtn.id = 'leftBtn';
+// leftBtn.id = 'rightBtn';
 
-/////////// CreateTextNode///////////
-// Navbar
-navText.innerText = "t3n.de";
-navButton.innerText = "Login";
-// Pioneers Network
-pioneersH1.innerText = "PIONEERS NETWORK";
-pioneersH2.innerHTML = "Gestalte mit uns eine positive digitale Zukunft";
-pioneersButton.innerHTML = "Jetzt kostenlos anmelden";
-//Slider
-sliderH2.innerHTML = "Finde andere digitale pioniere"
+// /////////// CreateTextNode///////////
+// // Navbar
+// navText.innerText = "t3n.de";
+// navButton.innerText = "Login";
+// // Pioneers Network
+// pioneersH1.innerText = "PIONEERS NETWORK";
+// pioneersH2.innerHTML = "Gestalte mit uns eine positive digitale Zukunft";
+// pioneersButton.innerHTML = "Jetzt kostenlos anmelden";
+// //Slider
+// sliderH2.innerHTML = "Finde andere digitale pioniere"
 
-////////////// AppendChild /////////////
-document.body.appendChild(nav);
-nav.appendChild(navLogoDiv);
-nav.appendChild(navLoginDiv);
-navLogoDiv.appendChild(navLogoFoto);
-navLoginDiv.appendChild(navText);
-navLoginDiv.appendChild(navButton);
-// Pioneers Network
-document.body.appendChild(pioneersDiv);
-pioneersDiv.appendChild(pioneersH1);
-pioneersDiv.appendChild(pioneersH2);
-pioneersDiv.appendChild(pioneersButton);
-//Slider
-document.body.appendChild(sliderDefaultDiv);
-sliderDefaultDiv.appendChild(sliderH2);
-sliderDefaultDiv.appendChild(sliderDiv);
+// ////////////// AppendChild /////////////
+// document.body.appendChild(nav);
+// nav.appendChild(navLogoDiv);
+// nav.appendChild(navLoginDiv);
+// navLogoDiv.appendChild(navLogoFoto);
+// navLoginDiv.appendChild(navText);
+// navLoginDiv.appendChild(navButton);
+// // Pioneers Network
+// document.body.appendChild(pioneersDiv);
+// pioneersDiv.appendChild(pioneersH1);
+// pioneersDiv.appendChild(pioneersH2);
+// pioneersDiv.appendChild(pioneersButton);
+// //Slider
+// document.body.appendChild(sliderDefaultDiv);
+// sliderDefaultDiv.appendChild(sliderH2);
+// sliderDefaultDiv.appendChild(sliderDiv);
 
 window.onload = function () {
     this.console.log('Dokument geladen');
     initBecomePionier();
-    initFaq();
-    initFooter();
+    // initFaq();
+    // initFooter();
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -128,45 +128,78 @@ for (i = 0; i < 25; i++) {
 }
 
 //einfügung die Profiles zu SliderDiv
-for (i = 0; i < sliderProfiles.length; i++) {
+// for (i = 0; i < sliderProfiles.length; i++) {
 
-    const freeDiv = document.createElement("div");
-    const freeA = document.createElement("a");
-    const Img = document.createElement("img");
-    const name = document.createElement("h3");
-    const position = document.createElement("p");
-    const company = document.createElement("p");
+//     const freeDiv = document.createElement("div");
+//     const freeA = document.createElement("a");
+//     const Img = document.createElement("img");
+//     const name = document.createElement("h3");
+//     const position = document.createElement("p");
+//     const company = document.createElement("p");
 
-    freeDiv.appendChild(freeA);
-    freeA.appendChild(Img);
-    freeA.appendChild(name);
-    freeA.appendChild(position);
-    freeA.appendChild(company);
+//     freeDiv.appendChild(freeA);
+//     freeA.appendChild(Img);
+//     freeA.appendChild(name);
+//     freeA.appendChild(position);
+//     freeA.appendChild(company);
 
-    freeDiv.className = "profilInfos";
-    freeA.href = '#';
-    Img.src = sliderProfiles[i].img;
-    name.innerHTML = sliderProfiles[i].name;
-    position.innerHTML = sliderProfiles[i].workingPosition;
-    company.innerHTML = sliderProfiles[i].company;
-    company.id = "companyImg";
-    position.id = 'position';
+//     freeDiv.className = "profilInfos";
+//     freeA.href = '#';
+//     Img.src = sliderProfiles[i].img;
+//     name.innerHTML = sliderProfiles[i].name;
+//     position.innerHTML = sliderProfiles[i].workingPosition;
+//     company.innerHTML = sliderProfiles[i].company;
+//     company.id = "companyImg";
+//     position.id = 'position';
 
-    slider.appendChild(freeDiv);
-}
-sliderDiv.appendChild(slider);
+//     slider.appendChild(freeDiv);
+// }
+// sliderDiv.appendChild(slider);
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////Become Pionier//////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
-/*
-url(https://storage.googleapis.com/t3n-de/pioneers/28a287d4da97bf4bdc65935dcb96bcc29a05e33f/t3n_Background_Texture.gif)
-*/
-
 const initBecomePionier = function () {
+    const outerDiv = document.createElement('div');
+    outerDiv.id = 'becomePionier';
+    const span = document.createElement('span');
+    span.innerHTML = 'J';
+    const hAr = ["ETZT", "PIONIER","WERDEN"];
+    const sectionHeading = document.createElement('div')
+    sectionHeading.appendChild(span);
+        hAr.forEach(e => {
+            const h = document.createElement('h2');
+            h.innerHTML= e;
+            h.className = 'heading-head';
+            sectionHeading.appendChild(h)
+        });
+    // sectionHeading.innerHTML= 'JETZT PIONIER WERDEN';
+    sectionHeading.className = 'becomePionier-Heading';
+    outerDiv.appendChild(sectionHeading);
+    outerDiv.appendChild(getMovementSection());
+    document.body.appendChild(outerDiv);
+}
 
+const getMovementSection = function() {
+    const area = document.createElement('div');
+    area.className = 'becomePionier-textArea';
+    const areaHeading = document.createElement('h4');
+    areaHeading.innerHTML = 'Werde Teil der Bewegung!';
+    areaHeading.className = ('textArea-heading');
+    const p = document.createElement('p');
+    p.innerHTML = 'Zeig, wer du bist und werde Mitglied im Pioneers Network – dem Netzwerk für Menschen, die die Digitalisierung gestalten. Menschen, die etwas bewegen wollen, die kreativ sind, die eine Haltung haben, die positiv denken, konstruktives Feedback und Impulse geben und Spaß bei der Arbeit haben. Digitale Pioniere eben.';
+    p.className ='textArea-text';
+    const b = document.createElement('button');
+    b.className = 'textArea-button';
+    b.innerHTML ='Jetzt kostenlos anmelden';
+    area.appendChild(areaHeading);
+    area.appendChild(p)
+    area.appendChild(b);
+    return area;
+
+    
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
