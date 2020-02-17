422ec54166573205886c09b07ba90db478cbc7ce
sungro <sungro.lee@empiriecom.com>
2020-02-17 11:28:09 +0100

    Login.html Problem checking

--- js/login.js
+++ js/login.js
@@ -1,5 +1,4 @@
 import * as Footer from './footer.js';
-<<<<<<< HEAD
 import * as Nav from './nav.js';
 
 
@@ -7,11 +6,6 @@ import * as Nav from './nav.js';
 window.onload = () => {
     Nav.initNav();
     // Headers.init();
-=======
-import * as Header from './nav.js';
-window.onload = () => {
-    Header.initNav();
->>>>>>> e0895bc94c0f64895380e7043daed0f49a008439
     initRegister();
     initPreFooter();
     Footer.init();
