e9b1832cb5beff66a55fb0436b20472013c49778
Danz <kaiadrian.danz@baur.de>
2020-02-20 14:38:30 +0100

    click handling register-page

--- js/login.js
+++ js/login.js
@@ -26,31 +26,12 @@ function initRegister() {
     form.appendChild(orLoginParagraph);
 
     const mail = createInputField({ label: 'E-Mail-Adresse', inputType: 'text', infoName: 'emailadress', errorparagrapId: 'mailError' });
-    // form.appendChild(mail.label);
-    // form.appendChild(mail.input);
-    // form.appendChild(mail.cancelInputButton)
-    // form.appendChild(mail.errorParagraph);
     form.appendChild(mail);
-
     const preName = createInputField({ label: 'Vorname', inputType: 'text', infoName: 'vorname', errorparagrapId: 'preNameError' });
-    // form.appendChild(preName.label);
-    // form.appendChild(preName.input)
-    // form.appendChild(preName.cancelInputButton);
-    // form.appendChild(preName.errorParagraph);
     form.appendChild(preName);
-    
     const name = createInputField({ label: 'Nachname', inputType: 'text', infoName: 'nachname', errorparagrapId: 'nameError' })
-    // form.appendChild(name.label)
-    // form.appendChild(name.input);
-    // form.appendChild(name.cancelInputButton);
-    // form.appendChild(name.errorParagraph);
     form.appendChild(name);
-    
     const password = createInputField({ label: 'Passwort', inputType: 'password', infoName: 'password', errorparagrapId: 'passwordError' });
-    // form.appendChild(password.label);
-    // form.appendChild(password.input);
-    // form.appendChild(password.cancelInputButton)
-    // form.appendChild(password.errorParagraph);
     form.appendChild(password);
 
     const captcha = getCaptcha();
@@ -78,88 +59,88 @@ function initRegister() {
     container.appendChild(form)
     document.body.appendChild(container);
 }
-function createInputField(parameter) {
+function createInputField(probs) {
     const groupingDiv = document.createElement('div');
     const label = document.createElement('label');
     const input = document.createElement('input');
-    const cancelInputButton = document.createElement('div');
     const span = document.createElement('span');
     const errorParagraph = document.createElement('p');
 
-    groupingDiv.id = parameter.infoName;
-    label.innerHTML = parameter.label;
-    // input.setAttribute('hasContent', 'false');
-    input.type = parameter.inputType;
-    input.name = parameter.infoName;
-    // input.id = parameter.infoName;
-    cancelInputButton.className = 'input-cancel';
-    // cancelInputButton.setAttribute('state', 'inactive');
-    // span.setAttribute('state', 'inactive');
-    // span.setAttribute('type', parameter.inputType);
-    // span.classList = 'invisible'
-    errorParagraph.id = parameter.errorparagrapId;
-
-    cancelInputButton.appendChild(span);
+    groupingDiv.id = probs.infoName;
+    label.innerHTML = probs.label;
+    input.type = probs.inputType;
+    input.name = probs.infoName;
+    errorParagraph.id = probs.errorparagrapId;
+    span.classList.add('empty')
 
     groupingDiv.appendChild(label)
     groupingDiv.appendChild(input)
-    groupingDiv.appendChild(cancelInputButton)
+    groupingDiv.appendChild(span)
     groupingDiv.appendChild(errorParagraph)
 
+    groupingDiv.onclick = function () {
+        onClickClassHandler(groupingDiv)
+    }
 
-
-    if (parameter.inputType == 'password') {
-        // span.id='passwordChanger'
-        // span.classList='invisible'
-        // input.onclick = function() {
-        //     input.setAttribute('state','active');
-        // }
-    } else {
-        input.onfocus = function(){
-            // input.parentElement.classList.add('active')
-        }
-        input.onclick = function () {
-            // span.classList.add('active');
-            
-            // document.getElementById('password').setAttribute('state','inactive');
-            // input.setAttribute('state', 'active');
-            // span.setAttribute('isActive', 'true');
-        }
+    input.onfocus = function () {
+        activInputButtonClassHandler(input);
     }
     input.oninput = function () {
-        inputWatcher(input);
+        onInputClassHandler(input);
     }
     input.onblur = function () {
-        // span.classList.replace('active','inactive')
-        inputVerifier(input)
-        input.oninput = function(){
-            inputVerifier(input);
-        }
-        if (parameter.inputType != 'password') {
-            // input.setAttribute('state', 'inactive')
-            // span.setAttribute('isActive', 'false');
-
-        }
+        onBlurHandler(input);
     };
 
-    if (parameter.inputType === 'password') {
+    if (probs.inputType === 'password') {
+        const inputElement = span.parentNode.childNodes[1];
         span.addEventListener('click', function () {
-            input.type == 'password' ? input.type = 'text' : input.type = 'password';
-        });
+            inputElement.type == 'text' ? inputElement.type = 'password' : inputElement.type = 'text';
+        })
     } else {
-        cancelInputButton.addEventListener('click', function () { clearInput(input) });
         span.addEventListener('click', function () { clearInput(input) });
 
     }
-    // setupHandler(groupingDiv);
-    // return {
-        // label, input, cancelInputButton, errorParagraph
-        // };
-       return groupingDiv
+    return groupingDiv
 }
-// function setupHandler(elements){
-//     elements.
-// }
+
+function onClickClassHandler(div) {
+    activeClassRemover()
+    div.classList.replace('inactive', 'active');
+}
+function activeClassRemover() {
+    const allDivs = Array.from(document.getElementsByTagName('form')[0].getElementsByTagName('div'));
+    allDivs.forEach(div => {
+        if (div.classList.contains('active')) {
+            div.classList.replace('active', 'inactive');
+        }
+    });
+}
+function onInputClassHandler(input) {
+    inputWatcher(input);
+
+}
+function onBlurHandler(input) {
+    inputVerifier(input)
+    input.oninput = function () {
+        inputWatcher(input)
+        inputVerifier(input);
+    }
+}
+function activInputButtonClassHandler(input) {
+    const switchCondition = input.name;
+    input.parentElement.classList.add('active');
+    if (switchCondition.includes('password')) {
+    } else {
+        if (input.value.length > 0) {
+            input.nextElementSibling.classList.add('containing')
+        } else {
+            input.parentElement.classList.replace('containing', 'empty')
+            input.nextElementSibling.classList.add('empty');
+        }
+    }
+}
+
 function getCaptcha() {
     const script = document.createElement('script');
     script.src = 'https://www.google.com/recaptcha/api.js?render=_reCAPTCHA_site_key';
@@ -185,27 +166,26 @@ function clearInput(inputField) {
     inputField.value = '';
     inputWatcher(inputField);
 }
+function getCrossElement(input) {
+    return input.nextElementSibling;
+}
 function inputWatcher(inputField) {
-    if (inputField.value != '' && inputField.getAttribute('hasContent') === 'false') {
-        inputField.setAttribute('hasContent', 'true');
-        inputField.nextElementSibling.childNodes[0].setAttribute('state', 'active')
-        inputField.nextElementSibling.setAttribute('state', 'active')
-    }
-    if (inputField.value == '' && inputField.getAttribute('hasContent') === 'true') {
-        inputField.setAttribute('hasContent', 'false');
-        inputField.nextElementSibling.childNodes[0].setAttribute('state', 'inactive')
-        inputField.nextElementSibling.setAttribute('state', 'inactive')
+    if (inputField.value.length > 0) {
+        const cross = getCrossElement(inputField);
+        cross.classList.add('containing')
+        cross.classList.remove('empty');
+    } else {
+        getCrossElement(inputField).classList.remove('containing')
+        getCrossElement(inputField).classList.add('empty')
     }
 }
 function inputVerifier(inputField) {
-    console.log(inputField);
     var fieldObject;
     if (inputField.name.includes('mail')) {
         fieldObject = { field: inputField, errorparagraph: getErrorParagraph('mailError') };
         verifyMailadressInput(inputField) ? acceptInputThrough(fieldObject) : declineInputThrough(fieldObject);
     } else if (!inputField.name.includes('password')) {
         if (inputField.name.includes('vor')) {
-            console.log("vorname")
             fieldObject = { field: inputField, errorparagraph: getErrorParagraph('preNameError') }
             verifyNameInput(fieldObject) ? acceptInputThrough(fieldObject) : declineInputThrough(fieldObject);
         } else {
@@ -236,27 +216,22 @@ function verifyMailadressInput(inputField) {
     return regex.test(inputField.value);
 }
 function verifyNameInput(inputObject) {
-    console.log("verifyer")
     return inputObject.field.value.length >= 3;
 }
 function createInputError(errorObject) {
-    // console.log(errorObject)
-    // console.log(errorObject.field.name)
     var errorMessage;
-    if(!errorObject.field.classList.value.includes('error')){
+    if (!errorObject.field.classList.value.includes('error')) {
         errorObject.field.classList += ' error';
     }
     errorObject.errorparagraph.classList = 'visible';
     switch (errorObject.field.name) {
         case 'emailadress':
-            errorObject.field.value.length !== 0 ? errorMessage =  'Bitte gib eine gültige E-Mail-Adresse an.': errorMessage = 'Bitte gib deine E-Mail-Adresse an.'
+            errorObject.field.value.length !== 0 ? errorMessage = 'Bitte gib eine gültige E-Mail-Adresse an.' : errorMessage = 'Bitte gib deine E-Mail-Adresse an.'
             break;
         case 'vorname':
-            console.log("CreateInputError -> name")
             errorObject.field.value.length == 0 ? errorMessage = 'Bitte gib deinen Vornamen an.' : errorMessage = 'Der Vorname muss mindestens 3 Zeichen lang sein.'
             break;
         case 'nachname':
-            console.log("nachname");
             errorObject.field.value.length == 0 ? errorMessage = 'Bitte gib deinen Nachnamen an.' : errorMessage = 'Der Nachname muss midnestens 3 Zeichen lang sein.'
             break;
         case 'password':
