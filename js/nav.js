1c510a0e54440c27e0e6f03975b565adfab0b20c
sungro <sungro.lee@empiriecom.com>
2020-02-24 11:19:05 +0100

    Review bearbeitung

--- js/nav.js
+++ js/nav.js
@@ -2,23 +2,29 @@ import * as SetElement from "./setElement.js";
 
 const initNav = () => {
 
+    const header = SetElement.setElements("header");
     const nav = SetElement.setElements("nav");
     const navLogoDiv = SetElement.setElements("div", "nav_logoDiv");
     const navLoginDiv = SetElement.setElements("div", "nav_loginDiv");
     const navLogoFoto = SetElement.setElements("img", "nav_logoFoto");
     const navText = SetElement.setElements("a", "nav_t3n");
     const navButton = SetElement.setElements("a", "nav_loginButton");
+    const imgAnchor = SetElement.setElements("a");
 
     navLogoFoto.src = "./foto/t3n-logo.png";
     navText.href = '#';
     navButton.href = '#';
     navText.innerText = "t3n.de";
     navButton.innerText = "Login";
+    imgAnchor.href = '../index.html';
 
-    document.body.appendChild(nav);
+    document.body.appendChild(header);
+    header.appendChild(nav);
     nav.appendChild(navLogoDiv);
     nav.appendChild(navLoginDiv);
-    navLogoDiv.appendChild(navLogoFoto);
+    imgAnchor.appendChild(navLogoFoto);
+    navLogoDiv.appendChild(imgAnchor);
+    // navLogoDiv.appendChild(navLogoFoto);
     navLoginDiv.appendChild(navText);
     navLoginDiv.appendChild(navButton);
 }
